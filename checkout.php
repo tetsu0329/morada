63c58ba2ba01fae8f52015e408641de31c405bea
Louise Eydriane Miranda <letmirandaaa@gmail.com>
2018-08-21 20:33:01 +0800

    added checkout process page

--- checkout.php
+++ checkout.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Gallery</title>
+	<title>Checkout</title>
 </head>
 <style type="text/css">
 	/*products*/
@@ -30,13 +30,152 @@
 	.viewclose:hover{
 		background: #ceaea5 !important;
 	}
+
+		.mybtn{
+		border-radius: 5px !important;
+		border: none;
+	}
+	.mybtn:hover{
+		background: transparent !important;
+		border: 2px solid #8C6A48;
+		color: #8C6A48 !important;
+	}
+
+	th{
+		background: #8C6A48 !important;
+		color: #fff !important;
+	}
+	.total{
+		background: #8C6A48 !important;
+		color: #fff !important;
+	}
 </style> 	
 <body>
-
-	<!-- our products -->
 	<div class="w3-container w3-light-grey w3-row w3-center" style="padding: 100px 0px 0px 0px;">
     <span class="w3-xxlarge">Checkout</p>
   	</div>
+  	<br>
+  	 <div class="w3-center">
+    	<h6>Please check your orders below.</h6>
+  </div>
+  
+  	<div class="w3-container">
+<div class="w3-responsive w3-card-4">
+<table class="w3-table w3-striped w3-bordered w3-hoverable">
+<thead>
+<tr class="w3-theme">
+  <th>Product Code</th>
+  <th>Product Name</th>
+  <th>Quantity</th>
+  <th>Price</th>
+  <th>Subtotal</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+  <td>001</td>
+  <td>Lorem Ipsum Product Name</td>
+  <td>1</td>
+  <td>123.00</td>
+  <td>123.00</td>
+</tr>
+
+<tr>
+  <td>001</td>
+  <td>Lorem Ipsum Product Name</td>
+  <td>1</td>
+  <td>123.00</td>
+  <td>123.00</td>
+</tr>
+
+<tr>
+  <td>001</td>
+  <td>Lorem Ipsum Product Name</td>
+  <td>1</td>
+  <td>123.00</td>
+  <td>123.00</td>
+</tr>
+
+<tr>
+  <td>001</td>
+  <td>Lorem Ipsum Product Name</td>
+  <td>1</td>
+  <td>123.00</td>
+  <td>123.00</td>
+</tr>
+
+<tr>
+  <td>001</td>
+  <td>Lorem Ipsum Product Name</td>
+  <td>1</td>
+  <td>123.00</td>
+  <td>123.00</td>
+</tr>
+<!-- total -->
+<tr class="total">
+  <td><b>TOTAL</b></td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td><b>123.00</b></td>
+</tr>
+<!-- total -->
+
+
+</tbody>
+</table>
+</div>
+<br>
+</div>
+
+
+<center><p><a href="checkout.php" class="w3-button w3-brown mybtn">CONFIRM ORDER</a></p></center>
+<br>
+
+ <div class="w3-center">
+    	<h6>Please choose your payment option.</h6>
+  </div>
+<br>
+	<div class="w3-container w3-light-grey" style="padding: 50px 50px 50px 50px;">
+	  <div class="w3-row-padding">
+
+	  	<div class="w3-col m6 w3-center">
+	  		<input id="male" class="w3-radio" type="radio" name="payment" value="paypal" checked>
+    <label>Paypal</label>
+    <br>
+    <br>
+	      <img class="w3-image" src="img/paypal.png" style="width: 300px; padding-top: 5%;">
+	    </div>
+
+	    <div class="w3-col m6 w3-center">
+	  		<input id="male" class="w3-radio" type="radio" name="payment" value="cod" checked>
+    <label>Cash on Delivery</label>
+    <br>
+    <br>
+	      <img class="w3-image" src="img/cash.png" style="width: 300px; padding-top: 5%;">
+	    </div>
+
+	  </div>
+	</div>
+<!-- <div style="margin-bottom: 5.5%;"></div> -->
+<!-- scripts -->
+	<script type="text/javascript">
+	
+    var count = 0;
+    var countEl = document.getElementById("count");
+    function plus(){
+        count++;
+        countEl.value = count;
+    }
+    function minus(){
+      if (count >0 ) {
+        count--;
+        countEl.value = count;
+      }  
+    }
+
+</script>
+<!-- scripts -->
 </body>
 </html>
 <?php include ('footer.php');?>
