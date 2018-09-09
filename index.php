52c15cc1ec587f70b2df20588e354a56079d20da
Rey Lumagui <reylumagui0329@gmail.com>
2018-09-09 16:45:47 +0800

    Fixed featured bug

--- index.php
+++ index.php
@@ -185,7 +185,7 @@
 		?>
 	    <div class="w3-col l3 m6 w3-margin-bottom boxlink">
 		    <a href="product_view.php"><div class="w3-card boxprod">
-		      <img src="<?php echo $rows['productimage'] ?>" alt="John" style="width:100%; height:250px">
+		      <img src="<?php echo substr($rows['productimage'],3) ?>" alt="John" style="width:100%; height:250px">
 		        <div class="w3-container">
 		          <h3><?php echo $rows['productname'] ?></h3>
 		          <h6 class="prodprice">P <?php echo $rows['itemprice'] ?></h6>
